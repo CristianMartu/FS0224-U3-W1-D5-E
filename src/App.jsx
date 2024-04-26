58f7f7d6d737c4eb130d8ea6029d17f87ddf6d70
CristianMartu <martuccicristian18@gmail.com>
2024-04-26 23:08:53 +0200

    extra

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ import './App.css'
 import MyNavbar from './components/MyNav'
 import MyFooter from './components/MyFooter'
 import MyMain from './components/MyMain'
+import MyEdit from './components/MyEdit'
+import MySettings from './components/MySettings'
 
 function App() {
   return (
+    // Netflix HopePage
     <div className="App">
       <MyNavbar />
       <MyMain />
       <MyFooter />
     </div>
+
+    // Netflix Edit
+    // <div className="App">
+    //   <MyEdit />
+    // </div>
+
+    // Netflix Settings
+    // <div className="App">
+    //   <MyNavbar />
+    //   <MySettings />
+    // </div>
   )
 }
 
